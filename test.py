8481d47e6b5211d2e78e48ead26bf0c2cbc9f62b
dumami250015 <khoivotri@gmail.com>
2025-11-17 09:39:15 +0700

    BinaryClassification

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn import linear_model
+from sklearn.metrics import accuracy_score
+from scipy import misc
+np.random.seed(0)
+
+train_ids = np.arange(1, 26)
+print(train_ids)
